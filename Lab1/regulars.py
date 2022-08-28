9b550953668ab9ed10ecdf7901c6e3c2ee2373a0
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-08-28 14:53:45 +0300

    k

--- Lab1/regulars.py
+++ Lab1/regulars.py
@@ -10,7 +10,7 @@ print("there are " + str(word) + " words in text")
 s = "fhsdgfsdgfhsdfg AA1234AB jgsdhfgshggdf AE4536GH dgfhsdgfh AH6789HB"
 res = re.findall(r"\b[A-Z]{2}\d{4}[A-Z]{2}",s)
 print(res)
-region = dict{'AA':'Kyiv', 'AE':'Dnipropetrivsk obl', 'AH':'Donetsk'}
+region = {'AA':'Kyiv', 'AE':'Dnipropetrivsk obl', 'AH':'Donetsk'}
 for x in res:
-    reg = re.search(r"\w{2}")
-    print(region.get(reg))
+    reg = re.findall(r"\b\w{2}", x)
+    print(region.get(reg[0]))
