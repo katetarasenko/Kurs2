a17a55de6a9f0f8488a59f892dc4eb340697fc0d
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-08-28 15:44:15 +0300

    p

--- Lab1/regulars.py
+++ Lab1/regulars.py
@@ -7,16 +7,34 @@ print(res)
 print("there are " + str(word) + " words in text")
 
 #2 Розробити скрипт, який буде отримувати номер авто та визначати валідність номеру та регіон реєстрації. Номера, що можуть бути введені відповідають поточному українському законодавству (лише стандартні номери, номери на замовлення не використовуємо). Використайте лише 3 регіони реєстрації України.
+"""
 s = "fhsdgfsdgfhsdfg AA1234AB jgsdhfgshggdf AE4536GH dgfhsdgfh AH6789HB"
 res = re.findall(r"\b[A-Z]{2}\d{4}[A-Z]{2}",s)
 print(res)
 region = {'AA':'Kyiv', 'AE':'Dnipropetrivsk obl', 'AH':'Donetsk'}
 
-#matches = re.finditer(r"\b\w{2}", s)
-#for matchNum, match in enumerate(matches, start=1):
-#    print(match)
-
 for x in res:
     rr = str(x[0]+x[1])
     #reg = re.findall(r"\b\w{2}", x)l
-    print(str(x)+region.get(rr))
+    print(str(x) + " " +region.get(rr))
+"""
+
+region = {'AA':'Kyiv', 'AE':'Dnipropetrivsk obl', 'AH':'Donetsk'}
+#s = "fhsdgfsdgfhsdfg AA1234AB jgsdhfgshggdf AE4536GH dgfhsdgfh AH6789HB"
+s = "BA1234AB"
+
+pattern = r"\b[A-Z]{2}\d{4}[A-Z]{2}\b"
+res = re.findall(pattern, s)
+if res:
+    autonumber = res[0]
+    first2 = autonumber[0]+autonumber[1]
+    reg = region.get(first2)
+    if reg:
+        print(autonumber + " " + reg)
+    else:
+        print(first2 + " Not region exist")
+else:
+    print(s + " Not valid number")
+
+
+
