7a232992c417f84fbc59f268586dd2e6c68d126f
katetarasenko <kate.sweetpanter@gmail.com>
2022-09-19 21:53:42 +0300

    k

--- lab2_4/covid_csv.py
+++ lab2_4/covid_csv.py
@@ -6,20 +6,30 @@ import csv
 #3. Порахуйте, чи відповідають дані по загальній кількості випадків за 21.08.2020 по світу сумі випадків по країнах за цю дату?
 #4. Запишіть в текстовий файл найбільший показник по випадкам по кожній з країн. Н-д: Ukraine =)
 file = open("owid-covid-data.csv", "r")
-csv_reader = csv.reader(file)
-print(max_weekly_icu_admissions)
-i = 0
-for row in csv_reader:
-    print(row[0], '\t', row[5])
-    i += 1
-    print(i)
+# csv_reader = csv.reader(file)
+# i = 0
+# for row in csv_reader:
+#     print(row[0], '\t', row[5])
+#     i += 1
+# print(i)
 
-# dict_reader = csv.DictReader(file)
-# ukr_date = []
-# ukr_total_cases = []
-# week_increase = []
-# sum_21_08_2020 = 0
-# country_cases = {}
-# for row in dict_reader:
-#     if row.get('date') == '2020-08-30' and row.get('location') == 'Philippines':
-#          print('2020-08-30 on Philippines:'+row.get('total_cases'))
+dict_reader = csv.DictReader(file)
+ukr_date = []
+ukr_total_cases = []
+week_increase = []
+sum_21_08_2020 = 0
+country_cases = {}
+for row in dict_reader:
+    if row.get('date') == '2020-08-30' and row.get('location') == 'Philippines':
+         print('2020-08-30 on Philippines:'+row.get('total_cases'))
+    if row.get('iso_code') == 'UKR':
+        ukr_date.append(row.get('date'))
+        ukr_total_cases.append(int(float(row.get('total_cases'))))
+        print(row.get('total_cases'))
+print(ukr_total_cases)
+for i in  range(0,len(ukr_total_cases)-7):
+    week_increase.append(ukr_total_cases[i+7] - ukr_total_cases[i])
+    print(ukr_total_cases[i+7] - ukr_total_cases[i])
+print(week_increase)
+x = week_increase.index(max(week_increase))
+print('maximal increase of covid infection:'+ str(max(week_increase)) + 'in 7 days period from '+ ukr_date[x] +'-'+ukr_date[x+6])
